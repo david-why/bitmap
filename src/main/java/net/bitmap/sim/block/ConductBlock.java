ab1289b32230590b103c25816fb874474f8cfca6
david <david_why@outlook.com>
2020-07-03 19:15:31 +0800

    Quick commit.

--- src/main/java/net/bitmap/sim/block/ConductBlock.java
+++ src/main/java/net/bitmap/sim/block/ConductBlock.java
@@ -1,6 +1,7 @@
 package net.bitmap.sim.block;
 
 import net.fabricmc.fabric.api.event.player.AttackBlockCallback;
+import net.fabricmc.fabric.api.event.player.UseBlockCallback;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
@@ -17,6 +18,7 @@ import net.minecraft.util.Hand;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
+import net.minecraft.world.BlockView;
 import net.minecraft.world.World;
 
 public class ConductBlock extends Block {
@@ -24,19 +26,24 @@ public class ConductBlock extends Block {
     private static final IntProperty COLOR;
     private static final IntProperty ON;
     private static Integer speed = 2;
-    private static long lastAttack = 0;
+    private static long lastAttack = 0, lastUse = 0;
 
     public ConductBlock(Settings settings) {
         super(settings);
         AttackBlockCallback.EVENT.register(this::attackCallback);
+        UseBlockCallback.EVENT.register(this::useCallback);
     }
     public ConductBlock() {
         super(FabricBlockSettings.copy(Blocks.STONE));
         AttackBlockCallback.EVENT.register(this::attackCallback);
     }
 
-    public boolean activate(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
+    private ActionResult useCallback(PlayerEntity player, World world, Hand hand, BlockHitResult hit) {
+        if (!world.isClient || System.currentTimeMillis() - 300 < lastAttack) return ActionResult.PASS;
+        System.out.println("Activated...");
         Item itemInHand = player.getStackInHand(hand).getItem();
+        BlockPos pos = hit.getBlockPos();
+        BlockState state = world.getBlockState(pos);
         if (itemInHand == Items.GOLDEN_SWORD) {
             Integer onNow = (Integer)state.get(ON);
             if (onNow == 0 || onNow == 2) world.setBlockState(pos, (BlockState)state.cycle(ON), 3);
@@ -49,11 +56,11 @@ public class ConductBlock extends Block {
                 if (speed < 655360) {
                     speed *= 2;
                 }
-                else return false;
+                else return ActionResult.PASS;
             }
             player.sendMessage(new TranslatableText("message.speed." + speed.toString()));
-        } else return false;
-        return true;
+        } else return ActionResult.PASS;
+        return ActionResult.SUCCESS;
     }
 
     protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
@@ -62,9 +69,10 @@ public class ConductBlock extends Block {
     }
 
     private ActionResult attackCallback(PlayerEntity player, World world, Hand hand, BlockPos pos, Direction direction) {
+        if (!world.isClient) return ActionResult.PASS;
         Item itemInHand = player.getStackInHand(hand).getItem();
         BlockState state = world.getBlockState(pos);
-        if (!(state.getBlock() instanceof ConductBlock) || System.currentTimeMillis() - 500 < lastAttack) return ActionResult.PASS;
+        if (!(state.getBlock() instanceof ConductBlock) || System.currentTimeMillis() - 300 < lastAttack) return ActionResult.PASS;
         if (itemInHand == Items.GOLDEN_SWORD) {
             Integer onNow = (Integer)state.get(ON);
             if (onNow == 1) world.setBlockState(pos, (BlockState)state.cycle(ON), 3);
@@ -84,6 +92,15 @@ public class ConductBlock extends Block {
         } else return ActionResult.PASS;
         lastAttack = System.currentTimeMillis();
         return ActionResult.SUCCESS;
+    }
+
+    public int getWeakRedstonePower(BlockState state, BlockView view, BlockPos pos, Direction facing) {
+        return (Integer)state.get(ON) == 2 ? 15 : 0;
+    }
+    
+    public boolean emitsRedstonePower(BlockState state) {
+        System.out.println((Integer)state.get(ON));
+        return (Integer)state.get(ON) == 2;
     }
 
     static {
