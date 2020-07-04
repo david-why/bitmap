daf4276a8da57a63a28693c8db305cde5befda12
david <david_why@outlook.com>
2020-07-04 13:37:06 +0800

    Fix code.

--- src/main/java/net/davidwhy/bitmap/BitMapMod.java
+++ src/main/java/net/davidwhy/bitmap/BitMapMod.java
@@ -13,21 +13,29 @@ import net.minecraft.util.registry.Registry;
 
 public class BitMapMod implements ModInitializer {
 
-    public static final Block YELLOW_SEMICONDUCTOR_BLOCK = new SemiConductorBlock(FabricBlockSettings.copy(Blocks.STONE));
-    public static final Block RED_SEMICONDUCTOR_BLOCK = new SemiConductorBlock(FabricBlockSettings.copy(Blocks.STONE));
-    public static final Block GREEN_SEMICONDUCTOR_BLOCK = new SemiConductorBlock(FabricBlockSettings.copy(Blocks.STONE));
-    public static final Block BLUE_SEMICONDUCTOR_BLOCK = new SemiConductorBlock(FabricBlockSettings.copy(Blocks.STONE));
+    public static final Block YELLOW_SEMICONDUCTOR_BLOCK = new SemiConductorBlock();
+    public static final Block RED_SEMICONDUCTOR_BLOCK = new SemiConductorBlock();
+    public static final Block GREEN_SEMICONDUCTOR_BLOCK = new SemiConductorBlock();
+    public static final Block BLUE_SEMICONDUCTOR_BLOCK = new SemiConductorBlock();
 
     @Override
     public void onInitialize() {
-        Registry.register(Registry.BLOCK, new Identifier("bitmap", "yellow_semiconductor_block"), YELLOW_SEMICONDUCTOR_BLOCK);
-        Registry.register(Registry.BLOCK, new Identifier("bitmap", "red_semiconductor_block"), RED_SEMICONDUCTOR_BLOCK);
-        Registry.register(Registry.BLOCK, new Identifier("bitmap", "green_semiconductor_block"), GREEN_SEMICONDUCTOR_BLOCK);
-        Registry.register(Registry.BLOCK, new Identifier("bitmap", "blue_semiconductor_block"), BLUE_SEMICONDUCTOR_BLOCK);
-        Registry.register(Registry.ITEM, new Identifier("bitmap", "yellow_semiconductor_block"), new BlockItem(YELLOW_SEMICONDUCTOR_BLOCK, new Item.Settings().group(ItemGroup.REDSTONE).maxCount(64)));
-        Registry.register(Registry.ITEM, new Identifier("bitmap", "red_semiconductor_block"), new BlockItem(RED_SEMICONDUCTOR_BLOCK, new Item.Settings().group(ItemGroup.REDSTONE).maxCount(64)));
-        Registry.register(Registry.ITEM, new Identifier("bitmap", "green_semiconductor_block"), new BlockItem(GREEN_SEMICONDUCTOR_BLOCK, new Item.Settings().group(ItemGroup.REDSTONE).maxCount(64)));
-        Registry.register(Registry.ITEM, new Identifier("bitmap", "blue_semiconductor_block"), new BlockItem(BLUE_SEMICONDUCTOR_BLOCK, new Item.Settings().group(ItemGroup.REDSTONE).maxCount(64)));
+        Registry.register(Registry.BLOCK, new Identifier("bitmap", "yellow_semiconductor_block"),
+                YELLOW_SEMICONDUCTOR_BLOCK);
+        Registry.register(Registry.BLOCK, new Identifier("bitmap", "red_semiconductor_block"), 
+                RED_SEMICONDUCTOR_BLOCK);
+        Registry.register(Registry.BLOCK, new Identifier("bitmap", "green_semiconductor_block"),
+                GREEN_SEMICONDUCTOR_BLOCK);
+        Registry.register(Registry.BLOCK, new Identifier("bitmap", "blue_semiconductor_block"),
+                BLUE_SEMICONDUCTOR_BLOCK);
+        Registry.register(Registry.ITEM, new Identifier("bitmap", "yellow_semiconductor_block"),
+                new BlockItem(YELLOW_SEMICONDUCTOR_BLOCK, new Item.Settings().group(ItemGroup.REDSTONE).maxCount(64)));
+        Registry.register(Registry.ITEM, new Identifier("bitmap", "red_semiconductor_block"),
+                new BlockItem(RED_SEMICONDUCTOR_BLOCK, new Item.Settings().group(ItemGroup.REDSTONE).maxCount(64)));
+        Registry.register(Registry.ITEM, new Identifier("bitmap", "green_semiconductor_block"),
+                new BlockItem(GREEN_SEMICONDUCTOR_BLOCK, new Item.Settings().group(ItemGroup.REDSTONE).maxCount(64)));
+        Registry.register(Registry.ITEM, new Identifier("bitmap", "blue_semiconductor_block"),
+                new BlockItem(BLUE_SEMICONDUCTOR_BLOCK, new Item.Settings().group(ItemGroup.REDSTONE).maxCount(64)));
     }
 
 }
--- src/main/java/net/davidwhy/bitmap/logic/SemiConductor.java
+++ src/main/java/net/davidwhy/bitmap/logic/SemiConductor.java
@@ -1,5 +1,25 @@
 package net.davidwhy.bitmap.logic;
 
 public class SemiConductor {
-    
+
+    private static int speed = 2;
+
+    public static int speedUp() {
+        if (speed == 2)
+            speed = 5;
+        else if (speed < 655360) {
+            speed *= 2;
+        }
+        return speed;
+    }
+
+    public static int speedDown() {
+        if (speed == 5)
+            speed = 2;
+        else if (speed > 1) {
+            speed /= 2;
+        }
+        return speed;
+    }
+
 }
