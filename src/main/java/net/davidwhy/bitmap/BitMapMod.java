8cac6308c459642d92a60987911bd872057b1ade
David <david_why@outlook.com>
2020-07-05 12:14:49 +0800

    Added MODID constant.

--- src/main/java/net/davidwhy/bitmap/BitMapMod.java
+++ src/main/java/net/davidwhy/bitmap/BitMapMod.java
@@ -11,6 +11,7 @@ import net.minecraft.util.registry.Registry;
 
 public class BitMapMod implements ModInitializer {
 
+	public static final String MODID = "bitmap";
     public static final Block YELLOW_SEMICONDUCTOR_BLOCK = new SemiConductorBlock();
     public static final Block RED_SEMICONDUCTOR_BLOCK = new SemiConductorBlock();
     public static final Block GREEN_SEMICONDUCTOR_BLOCK = new SemiConductorBlock();
@@ -18,21 +19,21 @@ public class BitMapMod implements ModInitializer {
 
     @Override
     public void onInitialize() {
-        Registry.register(Registry.BLOCK, new Identifier("bitmap", "yellow_semiconductor_block"),
+        Registry.register(Registry.BLOCK, new Identifier(MODID, "yellow_semiconductor_block"),
                 YELLOW_SEMICONDUCTOR_BLOCK);
-        Registry.register(Registry.BLOCK, new Identifier("bitmap", "red_semiconductor_block"), 
+        Registry.register(Registry.BLOCK, new Identifier(MODID, "red_semiconductor_block"), 
                 RED_SEMICONDUCTOR_BLOCK);
-        Registry.register(Registry.BLOCK, new Identifier("bitmap", "green_semiconductor_block"),
+        Registry.register(Registry.BLOCK, new Identifier(MODID, "green_semiconductor_block"),
                 GREEN_SEMICONDUCTOR_BLOCK);
-        Registry.register(Registry.BLOCK, new Identifier("bitmap", "blue_semiconductor_block"),
+        Registry.register(Registry.BLOCK, new Identifier(MODID, "blue_semiconductor_block"),
                 BLUE_SEMICONDUCTOR_BLOCK);
-        Registry.register(Registry.ITEM, new Identifier("bitmap", "yellow_semiconductor_block"),
+        Registry.register(Registry.ITEM, new Identifier(MODID, "yellow_semiconductor_block"),
                 new BlockItem(YELLOW_SEMICONDUCTOR_BLOCK, new Item.Settings().group(ItemGroup.REDSTONE).maxCount(64)));
-        Registry.register(Registry.ITEM, new Identifier("bitmap", "red_semiconductor_block"),
+        Registry.register(Registry.ITEM, new Identifier(MODID, "red_semiconductor_block"),
                 new BlockItem(RED_SEMICONDUCTOR_BLOCK, new Item.Settings().group(ItemGroup.REDSTONE).maxCount(64)));
-        Registry.register(Registry.ITEM, new Identifier("bitmap", "green_semiconductor_block"),
+        Registry.register(Registry.ITEM, new Identifier(MODID, "green_semiconductor_block"),
                 new BlockItem(GREEN_SEMICONDUCTOR_BLOCK, new Item.Settings().group(ItemGroup.REDSTONE).maxCount(64)));
-        Registry.register(Registry.ITEM, new Identifier("bitmap", "blue_semiconductor_block"),
+        Registry.register(Registry.ITEM, new Identifier(MODID, "blue_semiconductor_block"),
                 new BlockItem(BLUE_SEMICONDUCTOR_BLOCK, new Item.Settings().group(ItemGroup.REDSTONE).maxCount(64)));
     }
 
